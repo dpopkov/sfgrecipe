0fc1b622ce4f85db3b0674896e67ebaf0f88f5b1
Denis Popkov <pkvdenis@gmail.com>
2021-11-23 15:47:29 +0300

    Add UnitOfMeasure (completing assignment).

---
+++ src/main/java/learn/sfg/sfgrecipe/domain/UnitOfMeasure.java
@@ -0,0 +1,23 @@
+package learn.sfg.sfgrecipe.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
